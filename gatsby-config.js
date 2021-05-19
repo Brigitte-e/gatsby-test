0f88e6dd314d03fa9d5b0fe22018a556b3d476e1
Anastasiia Khymko <anastasiya.khymko@gmail.com>
2021-05-19 12:20:54 +0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`
+// });
+
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -52,6 +58,33 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area:
+        apiToken: process.env.DATO_API_TOKEN,
+  
+        // The project environment to read from. Defaults to the primary environment:
+        // environment: `master`,
+  
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+  
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+  
+        // Custom API base URL (most don't need this)
+        apiUrl: 'https://site-api.datocms.com',
+  
+        // Setup locale fallbacks
+        // In this example, if some field value is missing in Italian, fall back to English
+        // localeFallbacks: {
+        //   it: ['en'],
+        // },
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
 return graphql(`
-    query AirtableQuery{
+    {
         allAirtable {
             edges {
                 node {
@@ -23,13 +23,31 @@ return graphql(`
                     }
                 }
             }
-        }
+        },
+        allDatoCmsContentItem {
+            edges {
+              node {
+                id
+                linksField {
+                  id
+                  option
+                  optionKey
+                }
+                lastName
+                certification
+                firstName
+                locale
+                name
+                year
+              }
+            }
+          }
     }`).then(res => {
         res.data.allAirtable.edges.forEach(({ node }) => {
             createPage({
                 path: `/posts/${node.id}`,
                 component: require.resolve("./src/templates/post/post.tsx"),
-                context: { post: node.data },
+                context: { post: node.data, res: res },
             })
         })
     })
